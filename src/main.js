54764c183ad01f1f6be78eee7913f75815bd84c1
corvuscorae <corvuscorae@gmail.com>
2024-10-28 09:14:02 -0700

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// debug with extreme prejudice
+"use strict"
+
+// game config
+let config = {
+    parent: 'phaser-game',
+    type: Phaser.CANVAS,
+    render: {
+        pixelArt: true  // prevent pixel art from getting blurred when scaled
+    },
+    fps: { forceSetTimeOut: true, target: 60 },   // ensure consistent timing across machines
+    width: 600,
+    height: 800,
+    scene: [LVL_1]
+}
+
+const game = new Phaser.Game(config);
